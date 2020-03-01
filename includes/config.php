235c4238f3d73ed2c85612b6fda69ac73b6ebbe8
shubhamchakrabarty <shubhamchakrabarty@hotmail.com>
2020-03-01 23:39:22 +0530

    updated config php once again

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ ob_start();
 session_start();
 $timezone = date_default_timezone_set("Asia/Kolkata");
 // $con = mysqli_connect("localhost", "root", "", "slotify");
-$con = mysqli_connect("ec2-18-213-176-229.compute-1.amazonaws.com", "pydtmzcnfiyxma", "448dd48361c31d79afa238b5dd539aba7fa08415b41625fa4f27c8bb2459b2fb", "d70duhgtd7sk7k");
+$con = mysqli_connect("remotemysql.com", "47TgZdCgwP", "pdk2bdzAri", "47TgZdCgwP");
 if (mysqli_connect_errno()) {
 	echo "Failed to connect: " . mysqli_connect_errno();
 }
