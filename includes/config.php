d64735e53a55df5efabbeca18880d59744cb46be
Shubhamchakrabarty <shubhamchakrabarty@hotmail.com>
2021-01-04 17:22:00 +0530

    Update config.php
    added credentials for freemysqlhosting.net

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ ob_start();
 session_start();
 $timezone = date_default_timezone_set("Asia/Kolkata");
 // $con = mysqli_connect("localhost", "root", "", "slotify");
-$con = mysqli_connect("remotemysql.com", "47TgZdCgwP", "pdk2bdzAri", "47TgZdCgwP");
+$con = mysqli_connect("sql12.freemysqlhosting.net", "sql12384993", "lMzxgxl4xc", "sql12384993");
 if (mysqli_connect_errno()) {
 	echo "Failed to connect: " . mysqli_connect_errno();
 }
